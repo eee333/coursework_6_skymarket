9411b9cf66100b73e6886351a88e8567cb1eefbf
Evgeny Eroshin <eee333@yandex.ru>
2022-04-01 15:02:05 +0300

    Used NestedSimpleRouter for Comments

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,16 +1,17 @@
 from django.urls import include, path
 
 # настройка роутов для модели
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
 from ads.views import AdViewSet, CommentViewSet
 
-ad_router = SimpleRouter()
-comment_router = SimpleRouter()
-ad_router.register("", AdViewSet, basename="ads")
-comment_router.register("comments", CommentViewSet, basename="comments")
+ad_router = routers.SimpleRouter()
+ad_router.register(r"ads", AdViewSet, basename="ads")
+
+comment_router = routers.NestedSimpleRouter(ad_router, r"ads", lookup="ad")
+comment_router.register(r"comments", CommentViewSet, basename="comments")
 
 urlpatterns = [
     path("", include(ad_router.urls)),
-    path("<int:ad_pk>/", include(comment_router.urls)),
+    path("", include(comment_router.urls)),
 ]
